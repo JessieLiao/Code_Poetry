a9b89b5a8a32da96be19c92096e297b8f01ae914
Jessie Liao <jessie.liao.0622@gmail.com>
2017-11-14 12:49:10 -0500

    added files

---
+++ Code_Poetry.py
@@ -0,0 +1,38 @@
+import random
+import time as i
+
+causes_me = '( '
+die = ') '
+to = '* '
+want = '^ '
+now = '  '
+since_my_code_is = ' '
+loading = '\n'
+
+grows ="  _,-._  "
+blooms =" / \_/ \ "
+was =      " >-(_)-< "
+worth_the=    "\n \_/ \_/ "
+time_in_my_room =       "\n   `-'   "
+
+def forever():
+	return random.choice("' ")
+def makes_me_want_to_cry():
+	return int(raw_input("How sad are you today? ==> "))
+def It(output):
+	print output
+def my_room(limit):
+	return range(0,limit)
+Crying = ''
+
+#Actual Poem
+Coding = makes_me_want_to_cry()
+It(causes_me + to + want + to + die)
+for i_have_been in my_room(Coding):
+	Crying += (now + forever() + now + since_my_code_is + forever() + loading)	
+never_stops = Crying
+It(never_stops + loading)
+It(grows)
+It(blooms)
+It(was + worth_the + time_in_my_room)
+
